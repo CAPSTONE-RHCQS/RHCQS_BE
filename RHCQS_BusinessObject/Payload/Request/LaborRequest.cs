b2797de2c93f6d8a130f97afc19d8b2828a38fce
Bhieukhang <112839851+Bhieukhang@users.noreply.github.com>
2024-11-02 15:11:07 +0700

    Up Labor Request \ Response

---
+++ RHCQS_BusinessObject/Payload/Request/LaborRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RHCQS_BusinessObject.Payload.Request
+{
+    public class LaborRequest
+    {
+        public string? Name { get; set; }
+
+        public double? Price { get; set; }
+
+        public DateTime? InsDate { get; set; }
+
+        public DateTime? UpsDate { get; set; }
+
+        public bool? Deflag { get; set; }
+
+        public string? Type { get; set; }
+    }
+}
---
+++ RHCQS_BusinessObject/Payload/Response/LaborResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RHCQS_BusinessObject.Payload.Response
+{
+    public class LaborResponse
+    {
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public double? Price { get; set; }
+
+        public bool? Deflag { get; set; }
+
+        public string? Type { get; set; }
+    }
+}
