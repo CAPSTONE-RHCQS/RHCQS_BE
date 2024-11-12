2ffbd2853444f0b1019bef68ca13d068111ee912
nvtiendev <115709279+nvtiendev@users.noreply.github.com>
2024-11-12 12:06:49 +0700

    [nvtiendev][add vndatetime and IsDraft field]

---
+++ RHCQS_BusinessObject/Helper/LocalDateTime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RHCQS_BusinessObject.Helper
+{
+    public class LocalDateTime
+    {
+        public static DateTime VNDateTime()
+        {
+            // Định nghĩa múi giờ Việt Nam
+            DateTime utcNow = DateTime.UtcNow;
+
+            // Đảm bảo giờ được chuyển về UTC+7 (bất kể giờ hiện tại là UTC nào)
+            return utcNow.AddHours(7);
+        }
+    }
+}
