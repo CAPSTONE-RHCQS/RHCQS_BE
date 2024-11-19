f1ec9227aee4d0f934dbade4651a5a883bda88a0
Bhieukhang <112839851+Bhieukhang@users.noreply.github.com>
2024-11-19 11:18:52 +0700

    Create MaterialSectionRequest.cs

---
+++ RHCQS_BusinessObject/Payload/Request/MateSec/MaterialSectionRequest.cs
@@ -0,0 +1,15 @@
+﻿using RHCQS_DataAccessObjects.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RHCQS_BusinessObject.Payload.Request.MateSec
+{
+    public class MaterialSectionRequest
+    {
+        public string? Name { get; set; }
+        public string Code { get; set; }
+    }
+}
