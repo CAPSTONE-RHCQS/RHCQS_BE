521f0b12b68a6f4f6c15123591b503c9704e3265
Bhieukhang <112839851+Bhieukhang@users.noreply.github.com>
2024-11-19 11:19:00 +0700

    Create SupplierUpdateRequest.cs

---
+++ RHCQS_BusinessObject/Payload/Request/Sup/SupplierUpdateRequest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RHCQS_BusinessObject.Payload.Request.Sup
+{
+    public class SupplierUpdateRequest
+    {
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? ConstractPhone { get; set; }
+        [JsonIgnore]
+        public string? ImgUrl { get; set; }
+
+        public bool? Deflag { get; set; }
+
+        public string? ShortDescription { get; set; }
+
+        public string? Description { get; set; }
+
+        [JsonIgnore] 
+        public IFormFile? Image { get; set; }
+    }
+}
