273e5018c4bd86e8336e1893bfb74afb6ada30ee
Bhieukhang <112839851+Bhieukhang@users.noreply.github.com>
2024-11-19 11:44:32 +0700

    Update MaterialSectionUpdateRequest.cs

--- RHCQS_BusinessObject/Payload/Request/MateSec/MaterialSectionUpdateRequest.cs
+++ RHCQS_BusinessObject/Payload/Request/MateSec/MaterialSectionUpdateRequest.cs
@@ -10,5 +10,6 @@ namespace RHCQS_BusinessObject.Payload.Request.MateSec
     public class MaterialSectionUpdateRequest
     {
         public string? Name { get; set; }
+        public string? Type { get; set; }
     }
 }
