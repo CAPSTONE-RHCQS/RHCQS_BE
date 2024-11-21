e9325567252dadf822d7c27b109a22791010c73e
ngandolh <nganttk002@gmail.com>
2024-11-21 22:26:02 +0700

    [ngan][Test] Basic signalR connect

--- RHCQS_BE/Extenstion/DependencyServices.cs
+++ RHCQS_BE/Extenstion/DependencyServices.cs
@@ -49,13 +49,13 @@ namespace RHCQS_BE.Extenstion
 
         public static IServiceCollection AddSignalRServices(this IServiceCollection services)
         {
-            var serviceProvider = services.BuildServiceProvider();
-            var configuration = serviceProvider.GetService<IConfiguration>();
+            //var serviceProvider = services.BuildServiceProvider();
+            //var configuration = serviceProvider.GetService<IConfiguration>();
 
-            services.AddSignalR().AddAzureSignalR(configuration["AzureSignalRConnectionString"]);
-            return services;
-            //services.AddSignalR();
+            //services.AddSignalR().AddAzureSignalR(configuration["AzureSignalRConnectionString"]);
             //return services;
+            services.AddSignalR();
+            return services;
         }
 
 
--- RHCQS_BE/Hubs/ChatHubcs.cs
+++ RHCQS_BE/Hubs/ChatHubcs.cs
@@ -32,60 +32,60 @@ namespace RHCQS_BE.Hubs
         private static Dictionary<string, List<string>> roomUsers = new();
 
 
-        //[Authorize]
-        //public async Task SendMessageToRoom(Guid roomId, string messageContent)
-        //{
-        //    var user = await _userService.GetCurrentLoginUser();
-        //    var userId = user.Userid;
-        //    if (userId == Guid.Empty)
-        //    {
-        //        throw new HubException("Invalid user ID.");
-        //    }
+        [Authorize]
+        public async Task SendMessageToRoom(Guid roomId, string messageContent)
+        {
+            var user = await _userService.GetCurrentLoginUser();
+            var userId = user.Userid;
+            if (userId == Guid.Empty)
+            {
+                throw new HubException("Invalid user ID.");
+            }
 
-        //    var room = await _messageService.GetChatRoomByIdAsync(roomId);
-        //    if (room == null)
-        //    {
-        //        throw new HubException("Room does not exist.");
-        //    }
+            var room = await _messageService.GetChatRoomByIdAsync(roomId);
+            if (room == null)
+            {
+                throw new HubException("Room does not exist.");
+            }
 
-        //    var createMessage = new MessageRequest
-        //    {
-        //        MessageContent = messageContent,
-        //        RoomId = roomId,
-        //        CreatedBy = userId
-        //    };
+            var createMessage = new MessageRequest
+            {
+                MessageContent = messageContent,
+                RoomId = roomId,
+                CreatedBy = userId
+            };
 
-        //    var message = await _messageService.CreateMessage(createMessage);
-        //    if (message.CreatedBy! == null)
-        //    {
-        //        return;
-        //    }
+            var message = await _messageService.CreateMessage(createMessage);
+            if (message.CreatedBy! == null)
+            {
+                return;
+            }
 
-        //    await Clients.Group(roomId.ToString()).SendAsync("ReceiveMessage", userId.ToString(), messageContent);
-        //}
-        //[Authorize]
-        //public async Task JoinRoom(Guid chatRoomId)
-        //{
-        //    var chatRoom = await _messageService.GetChatRoomByIdAsync(chatRoomId);
-        //    if (chatRoom == null)
-        //    {
-        //        throw new HubException("Invalid chatRoomId.");
-        //    }
+            await Clients.Group(roomId.ToString()).SendAsync("ReceiveMessage", userId.ToString(), messageContent);
+        }
+        [Authorize]
+        public async Task JoinRoom(Guid chatRoomId)
+        {
+            var chatRoom = await _messageService.GetChatRoomByIdAsync(chatRoomId);
+            if (chatRoom == null)
+            {
+                throw new HubException("Invalid chatRoomId.");
+            }
 
-        //    await Groups.AddToGroupAsync(Context.ConnectionId, chatRoomId.ToString());
+            await Groups.AddToGroupAsync(Context.ConnectionId, chatRoomId.ToString());
 
-        //    var messages = await _messageService.GetMessagesByChatRoomId(chatRoomId);
+            var messages = await _messageService.GetMessagesByChatRoomId(chatRoomId);
 
-        //    foreach (var message in messages)
-        //    {
-        //        await Clients.Caller.SendAsync("ReceiveMessage", message.CreatedBy.ToString(), message.Content);
-        //    }
-        //}
-        //[Authorize]
-        //public async Task LeaveRoom(Guid chatRoomId)
-        //{
-        //    await Groups.RemoveFromGroupAsync(Context.ConnectionId, chatRoomId.ToString());
-        //}
+            foreach (var message in messages)
+            {
+                await Clients.Caller.SendAsync("ReceiveMessage", message.CreatedBy.ToString(), message.Content);
+            }
+        }
+        [Authorize]
+        public async Task LeaveRoom(Guid chatRoomId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, chatRoomId.ToString());
+        }
 
         public async Task StartChatWithStaff(Guid customerId, Guid saleId, string initialMessage)
         {
