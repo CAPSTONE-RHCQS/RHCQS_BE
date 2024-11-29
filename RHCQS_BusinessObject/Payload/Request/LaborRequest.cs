537f0c2aba84168872510a859cdc83ca9f217301
Bhieukhang <112839851+Bhieukhang@users.noreply.github.com>
2024-11-29 16:44:23 +0700

    Update LaborRequest.cs

--- RHCQS_BusinessObject/Payload/Request/LaborRequest.cs
+++ RHCQS_BusinessObject/Payload/Request/LaborRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +10,13 @@ namespace RHCQS_BusinessObject.Payload.Request
     public class LaborRequest
     {
         public string? Name { get; set; }
-
+        [Range(0.01, double.MaxValue, ErrorMessage = "Giá phải lớn hơn 0.")]
         public double? Price { get; set; }
 
         public bool? Deflag { get; set; }
 
         public string? Type { get; set; }
+        [MaxLength(50, ErrorMessage = "Mã Code không được vượt quá 10 ký tự.")]
         public string? Code { get; set; }
     }
 }
