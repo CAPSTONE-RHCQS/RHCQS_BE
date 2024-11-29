2651935e4e713664d9a9f23dbf2c19c84dca68b8
Bhieukhang <112839851+Bhieukhang@users.noreply.github.com>
2024-11-29 20:51:21 +0700

    Update MaterialSectionUpdateRequest.cs

--- RHCQS_BusinessObject/Payload/Request/MateSec/MaterialSectionUpdateRequest.cs
+++ RHCQS_BusinessObject/Payload/Request/MateSec/MaterialSectionUpdateRequest.cs
@@ -1,6 +1,7 @@
 ﻿using RHCQS_DataAccessObjects.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace RHCQS_BusinessObject.Payload.Request.MateSec
     public class MaterialSectionUpdateRequest
     {
         public string? Name { get; set; }
-        public string? Type { get; set; }
+        [MaxLength(50, ErrorMessage = "Mã Code không được vượt quá 10 ký tự.")]
+        public string? Code { get; set; }
     }
 }
