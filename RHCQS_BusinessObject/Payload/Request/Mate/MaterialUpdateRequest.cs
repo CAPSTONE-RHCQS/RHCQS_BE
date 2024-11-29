d1f05ade0c9893dd78243b03bfddfb29a454d394
Bhieukhang <112839851+Bhieukhang@users.noreply.github.com>
2024-11-29 16:44:28 +0700

    Update MaterialUpdateRequest.cs

--- RHCQS_BusinessObject/Payload/Request/Mate/MaterialUpdateRequest.cs
+++ RHCQS_BusinessObject/Payload/Request/Mate/MaterialUpdateRequest.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ namespace RHCQS_BusinessObject.Payload.Request.Mate
         public Guid? SupplierId { get; set; }
         public Guid? MaterialSectionId { get; set; }
         public string? Name { get; set; }
+        [Range(0.01, double.MaxValue, ErrorMessage = "Giá phải lớn hơn 0.")]
         public decimal? Price { get; set; }
         public string? Unit { get; set; }
         public string? Size { get; set; }
@@ -21,6 +23,7 @@ namespace RHCQS_BusinessObject.Payload.Request.Mate
         public string? Description { get; set; }
         public bool? IsAvailable { get; set; }
         public string? UnitPrice { get; set; }
+        [MaxLength(50, ErrorMessage = "Mã Code không được vượt quá 10 ký tự.")]
         public string? Code { get; set; }
         [JsonIgnore]
         public IFormFile? Image { get; set; }
