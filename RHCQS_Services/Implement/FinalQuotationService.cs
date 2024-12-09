06addea5d667837f28396168448e116fc996c3c6
nvtiendev <115709279+nvtiendev@users.noreply.github.com>
2024-12-09 22:23:11 +0700

    [nvtiendev][uncomment when debug final]

--- RHCQS_Services/Implement/FinalQuotationService.cs
+++ RHCQS_Services/Implement/FinalQuotationService.cs
@@ -707,8 +707,8 @@ namespace RHCQS_Services.Implement
                             }
                     };
 
-                    //string dllPath = Path.Combine(AppContext.BaseDirectory, "ExternalLibraries", "libwkhtmltox.dll");
-                    //NativeLibrary.Load(dllPath);
+                    string dllPath = Path.Combine(AppContext.BaseDirectory, "ExternalLibraries", "libwkhtmltox.dll");
+                    NativeLibrary.Load(dllPath);
 
                     var pdf = _converter.Convert(doc);
                     //Upload cloudinary
