e4e7604defbce4e9e9b3f19ff66161e01cb4d9d3
Bhieukhang <112839851+Bhieukhang@users.noreply.github.com>
2024-12-13 11:36:52 +0700

    Create DesignPriceRequest.cs

---
+++ RHCQS_BusinessObject/Payload/Request/DesignPriceRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RHCQS_BusinessObject.Payload.Request
+{
+    public class DesignPriceRequest
+    {
+        [Range(0.01, double.MaxValue, ErrorMessage = "Diện tích nhập phải lớn hơn 0.")]
+        public double? AreaFrom { get; set; }
+        [Range(0.01, double.MaxValue, ErrorMessage = "Diên tích nhập phải lớn hơn 0.")]
+        public double? AreaTo { get; set; }
+        [Range(0.01, double.MaxValue, ErrorMessage = "Giá phải lớn hơn 0.")]
+        public double? Price { get; set; }
+    }
+}
