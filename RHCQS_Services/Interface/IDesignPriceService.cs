998cf22aec0e10f5884b5f8652ba46da9da10c9a
Bhieukhang <112839851+Bhieukhang@users.noreply.github.com>
2024-12-13 11:36:39 +0700

    Create IDesignPriceService.cs

---
+++ RHCQS_Services/Interface/IDesignPriceService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using RHCQS_BusinessObject.Payload.Request;
+using RHCQS_BusinessObject.Payload.Request.Mate;
+using RHCQS_BusinessObject.Payload.Response;
+using RHCQS_BusinessObjects;
+using RHCQS_DataAccessObjects.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RHCQS_Services.Interface
+{
+    public interface IDesignPriceService
+    {
+        Task<IPaginate<DesignPriceResponse>> GetListDesignPrice(int page, int size);
+        Task<DesignPriceResponse> GetDetailDesignPrice(Guid id);
+        Task<bool> CreateDesignPrice(DesignPriceRequest request);
+        Task<bool> UpdateDesignPrice(Guid id, DesignPriceRequest request);
+    }
+}
