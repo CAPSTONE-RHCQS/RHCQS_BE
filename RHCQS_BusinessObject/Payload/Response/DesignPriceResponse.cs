95fbbb0684627dc8670159027ff380d0664f0f2a
Bhieukhang <112839851+Bhieukhang@users.noreply.github.com>
2024-12-13 11:36:50 +0700

    Create DesignPriceResponse.cs

---
+++ RHCQS_BusinessObject/Payload/Response/DesignPriceResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RHCQS_BusinessObject.Payload.Response
+{
+    public class DesignPriceResponse
+    {
+        public Guid Id { get; set; }
+
+        public double? AreaFrom { get; set; }
+
+        public double? AreaTo { get; set; }
+
+        public double? Price { get; set; }
+
+        public DateTime? InsDate { get; set; }
+
+        public DateTime? UpsDate { get; set; }
+    }
+}
