6b332d3afba2a49c64c1d2ace5eccd47a1c42ad2
Bhieukhang <112839851+Bhieukhang@users.noreply.github.com>
2024-12-13 11:36:46 +0700

    Create DesignPrice.cs

---
+++ RHCQS_DataAccessObjects/Models/DesignPrice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RHCQS_DataAccessObjects.Models;
+
+public partial class DesignPrice
+{
+    public Guid Id { get; set; }
+
+    public double? AreaFrom { get; set; }
+
+    public double? AreaTo { get; set; }
+
+    public double? Price { get; set; }
+
+    public DateTime? InsDate { get; set; }
+
+    public DateTime? UpsDate { get; set; }
+
+    public virtual ICollection<Project> Projects { get; set; } = new List<Project>();
+}
